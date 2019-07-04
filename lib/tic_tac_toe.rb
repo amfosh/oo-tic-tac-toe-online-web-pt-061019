5e98437bd9667addb55625c3f33c5063da08929e
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-04 12:43:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,7 +66,8 @@ WIN_COMBINATIONS = [
   
   def won?
     WIN_COMBINATIONS.detect do |win_combination|
-      board[win_combination[0]] == board[win_combination[2]]
+      board[win_combination[0]] == board[win_combination[1]] &&
+      board[win_combination[2]] == board[win_combination[3]]
       true
     else
       false
